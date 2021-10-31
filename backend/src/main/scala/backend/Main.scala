f60d86dbb021fe6e27e78b2beb4cdf18fa3fe100
Vladimir Shevtsov <shevvv@tutanota.com>
2021-10-31 22:19:23 +0300

    [sc-22][sc-23]: WIP

--- backend/src/main/scala/backend/Main.scala
+++ backend/src/main/scala/backend/Main.scala
@@ -2,19 +2,20 @@ package backend
 
 import backend.resources.DatabaseProvider
 import zhttp.http._
-import zhttp.service.{ ChannelFactory, EventLoopGroup, _ }
 import zhttp.service.server.ServerChannelFactory
+import zhttp.service._
 import zio.{ App, ExitCode, URIO, ZIO }
 
 object Main extends App {
-
-  private val appEnv = DatabaseProvider.live ++ Logger.live
+  private val appEnv = DatabaseProvider.live ++ Log.live
   private val env = ServerChannelFactory.auto ++ ChannelFactory.auto ++ EventLoopGroup
     .auto() ++ appEnv
   private val server =
-    Server.port(9000) ++                // Setup port
-      Server.paranoidLeakDetection ++   // Paranoid leak detection (affects performance)
-      Server.app(CORS(Log(Routes.app))) // Setup the Http app
+    Server.port(9000) ++              // Setup port
+      Server.paranoidLeakDetection ++ // Paranoid leak detection (affects performance)
+      Server.app(
+        CORS(Logger <<< (Routes.public +++ Routes.publicM +++ (Auth <<< Routes.authed))),
+      )
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     server.make
